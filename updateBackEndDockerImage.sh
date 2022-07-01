7e36abf0e61f63cdcb26ea8411cc7037ca23df17
cmhill5 <79940226+cmhill5@users.noreply.github.com>
2022-07-01 15:14:56 -0500

    deployment commands for untested tag

--- updateBackEndDockerImage.sh
+++ updateBackEndDockerImage.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker build . -t p3-backend:untested
+docker build . -t elrintowser/p3-backend:untested
+docker push elrintowser/p3-backend:untested
+kubectl delete deployment backend-deployment -n p3-space
+kubectl apply -f /kubectl\ yml\ files/back-end-deployment.yml deployment -n p3-space
+kubectl rollout restart deployment backend-deployment -n p3-space
