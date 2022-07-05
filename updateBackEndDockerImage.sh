e124cec1ac95b2a26ae2e73715ebd83be4c00605
aarontdodge <aaron046@revature.net>
2022-07-05 08:05:22 -0600

    Modified kubernetes manifest for database persistent volume

--- updateBackEndDockerImage.sh
+++ updateBackEndDockerImage.sh
@@ -2,6 +2,5 @@
 
 docker build . -t elrintowser/p3-backend:untested
 docker push elrintowser/p3-backend:untested
-kubectl delete deployment backend-deployment -n p3-space
-kubectl apply -f /kubectl\ yml\ files/back-end-deployment.yml deployment -n p3-space
-kubectl rollout restart deployment backend-deployment -n p3-space
+kubectl apply -f "./kubectl yaml files/back-end-deployment.yml" -n p3-space
+# kubectl rollout restart deployment backend-deployment -n p3-space
