ffa4e1356123cfe20cfe94a4f317bc480782ba7a
cmhill5 <79940226+cmhill5@users.noreply.github.com>
2022-07-05 17:02:24 -0500

    back-end and application files change

--- updateBackEndDockerImage.sh
+++ updateBackEndDockerImage.sh
@@ -3,4 +3,5 @@
 docker build . -t elrintowser/p3-backend:untested
 docker push elrintowser/p3-backend:untested
 kubectl apply -f "./kubectl yaml files/back-end-deployment.yml" -n p3-space
+kubectl apply -f "./kubectl yaml files/back-end-service.yml" -n p3-space
 # kubectl rollout restart deployment backend-deployment -n p3-space
