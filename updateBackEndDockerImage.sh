ecda6001b06fde06586132cbf97e782337668cf3
ASchro1 <aaron243@revature.com>
2022-07-07 10:44:33 -0400

    bash script edited

--- updateBackEndDockerImage.sh
+++ updateBackEndDockerImage.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build . -t elrintowser/p3-backend:untested
-docker push elrintowser/p3-backend:untested
+docker build . -t elrintowser/p3-backend
+docker push elrintowser/p3-backend
 kubectl apply -f "./kubectl yaml files/back-end-deployment.yml" -n p3-space
 kubectl apply -f "./kubectl yaml files/back-end-service.yml" -n p3-space
 # kubectl rollout restart deployment backend-deployment -n p3-space
