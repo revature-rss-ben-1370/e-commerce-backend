c4094cf1ba3c1a0e917ca8dc0ccd573fecdc11a6
aarontdodge <aaron046@revature.net>
2022-07-07 14:31:58 -0600

    working on Micrometer

--- updateBackEndDockerImage.sh
+++ updateBackEndDockerImage.sh
@@ -4,4 +4,4 @@ docker build . -t elrintowser/p3-backend
 docker push elrintowser/p3-backend
 kubectl apply -f "./kubectl yaml files/back-end-deployment.yml" -n p3-space
 kubectl apply -f "./kubectl yaml files/back-end-service.yml" -n p3-space
-# kubectl rollout restart deployment backend-deployment -n p3-space
+kubectl rollout restart deployment back-end-deployment -n p3-space
