f7ffb79c6574f038b0103054ee9b758a7a7cddd9
ZacBurnett <zachary710@revature.net>
2022-07-12 07:53:56 -0700

    Merging changes into new branch.

---
+++ smoke-test.js
@@ -0,0 +1,14 @@
+import { sleep } from 'k6';
+import http from 'k6/http';
+
+export const options = {
+    duration: '2m',
+    vus: 20,
+};
+
+export default function () {
+    http.get('http://a24b3e0b0313345e49eb2e69fe33a185-35174312.us-east-1.elb.amazonaws.com/e-commerce/api/product');
+    sleep(0.01);
+    http.get('http://a24b3e0b0313345e49eb2e69fe33a185-35174312.us-east-1.elb.amazonaws.com/e-commerce/api/product');
+    sleep(0.01);
+}
